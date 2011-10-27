09880ba4932fa4994198aac71d24c092aa5a6340
Philipp Kuecuekyan <pkuecuekyan@phiand.co>
2011-10-27 19:36:46 -0400

    First commit

---
+++ NSString+Lexicon.h
@@ -0,0 +1,50 @@
+//
+//  NSString+Lexicon.h
+//
+//  Created by Philipp Kuecuekyan on 10/27/11.
+//  Copyright (c) 2011 phi & co. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ Adds convenience methods to NSString to take advantage of some of features of NSLinguisticTagger
+ namely to tag verbs, nouns, adjectives, pronouns and so on within the class string
+ */
+
+@interface NSString (Lexicon)
+
+/**
+ Break down string into grammatical components (noun, verb, adjective, pronoun, determiners)
+ @returns returns dictionary with arrays containing all components of a tag, respective tag is the key
+ */
+
+- (NSDictionary*) stringLexicon;
+
+/**
+ All verbs in the class string
+ @returns returns array of verbs, nil if none
+ */
+- (NSArray*) verbsInString;
+
+/**
+ All nouns in the class string
+ @returns returns array of nouns, nil if none
+ */
+- (NSArray*) nounsInString;
+
+/**
+ All pronouns in the class string
+ @returns returns array of pronouns, nil if none
+ */
+
+- (NSArray*) pronounsInString;
+
+/**
+ All adjectives in the class string
+ @returns returns array of adjectives, nil if none
+ */
+
+- (NSArray*) adjectivesInString;
+
+@end
---
+++ NSString+Lexicon.m
@@ -0,0 +1,95 @@
+//
+//  NSString+Lexicon.m
+//
+//  Created by Philipp Kuecuekyan on 10/27/11.
+//  Copyright (c) 2011 phi & co. All rights reserved.
+//
+
+#import "NSString+Lexicon.h"
+
+@implementation NSString (Lexicon)
+
+- (NSDictionary*) stringLexicon {
+    NSRange stringRange = NSMakeRange(0, (NSUInteger)[self length]);
+	
+    NSArray *language = [NSArray arrayWithObjects:@"en", nil];
+    NSDictionary* languageMap = [NSDictionary dictionaryWithObject:language forKey:@"Latn"];
+    
+    NSMutableDictionary *stringLexicon = [NSMutableDictionary dictionary];
+    NSMutableArray *nounArray = [NSMutableArray array];
+    NSMutableArray *verbArray = [NSMutableArray array];
+    NSMutableArray *pronounArray = [NSMutableArray array];
+    NSMutableArray *prepositionArray = [NSMutableArray array];
+	NSMutableArray *adjectiveArray = [NSMutableArray array];
+    NSMutableArray *determinerArray = [NSMutableArray array];
+    
+    [self enumerateLinguisticTagsInRange:stringRange 
+                                  scheme:NSLinguisticTagSchemeLexicalClass 
+                                 options:0
+                             orthography:[NSOrthography orthographyWithDominantScript:@"Latn" languageMap:languageMap]
+                              usingBlock:^(NSString *tag, NSRange tokenRange, NSRange sentenceRange, BOOL *stop) {
+                                  if ([tag isEqualToString:@"Noun"]) 
+                                      [nounArray addObject:[self substringWithRange:tokenRange]];
+                                  else if ([tag isEqualToString:@"Verb"]) 
+                                      [verbArray addObject:[self substringWithRange:tokenRange]];
+                                  else if ([tag isEqualToString:@"Pronoun"]) 
+                                      [pronounArray addObject:[self substringWithRange:tokenRange]];
+                                  else if ([tag isEqualToString:@"Preposition"])
+                                      [prepositionArray addObject:[self substringWithRange:tokenRange]];
+                                  else if ([tag isEqualToString:@"Determiner"])
+                                      [determinerArray addObject:[self substringWithRange:tokenRange]];
+                                  else if ([tag isEqualToString:@"Adjective"])
+                                      [adjectiveArray addObject:[self substringWithRange:tokenRange]];
+                                  
+                              }];
+    if ([nounArray count] > 0) {
+        [stringLexicon setValue:nounArray forKey:@"nouns"];
+    }
+    if ([verbArray count] > 0) {
+        [stringLexicon setValue:verbArray forKey:@"verbs"];
+    }
+    if ([adjectiveArray count] > 0) {
+        [stringLexicon setValue:adjectiveArray forKey:@"adjectives"];
+    }
+    if ([pronounArray count] > 0) {
+        [stringLexicon setValue:pronounArray forKey:@"pronouns"];
+    }
+    if ([prepositionArray count] > 0) {
+        [stringLexicon setValue:prepositionArray forKey:@"prepositions"];
+    }
+    if ([determinerArray count] > 0) {
+        [stringLexicon setValue:determinerArray forKey:@"determiners"];
+    }
+    return (NSDictionary*)stringLexicon;
+    
+}
+
+- (NSArray*) nounsInString {
+    NSArray *nouns = [[self stringLexicon] objectForKey:@"nouns"];
+    if (nouns != nil)
+        return nouns;
+    return nil;
+}
+
+- (NSArray*) verbsInString {
+    NSArray *verbs = [[self stringLexicon] objectForKey:@"verbs"];
+    if (verbs != nil)
+        return verbs;
+    return nil;
+}
+
+- (NSArray*) adjectivesInString {
+    NSArray *adjectives = [[self stringLexicon] objectForKey:@"adjectives"];
+    if (adjectives != nil)
+        return adjectives;
+    return nil;
+}
+
+- (NSArray*) pronounsInString {
+    NSArray *adjectives = [[self stringLexicon] objectForKey:@"adjectives"];
+    if (adjectives != nil)
+        return adjectives;
+    return nil;
+}
+
+@end
